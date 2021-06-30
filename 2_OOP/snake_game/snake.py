b2e76480037deefdbe0ebf329409cdd651f70d00
Dayeon Lee <iamdayeonlee@gmail.com>
2021-06-30 16:55:27 +0900

    Used Turtle Graphics module to practice OOP

---
+++ 2_OOP/snake_game/snake.py
@@ -0,0 +1,60 @@
+from turtle import Turtle
+import random
+STARTING_POSTION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT =0
+LIST_COLOR = ["red", "orange", "yellow", "blue", "green", "purple"]
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+        self.head.color(random.choice(LIST_COLOR))
+
+    def create_snake(self):
+        for position in STARTING_POSTION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color(random.choice(LIST_COLOR))
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+        # 2. move the snake
+    def move(self):
+            # snake move its body overlaping itself(2nd part's postion becomes 3rd part's new position)
+            for seg_num in range(len(self.segments) - 1, 0, -1):
+                new_x = self.segments[seg_num - 1].xcor()
+                new_y = self.segments[seg_num - 1].ycor()
+                self.segments[seg_num].goto(new_x, new_y)
+            self.head.forward(MOVE_DISTANCE)
+
+
+
+
+# 3. Control the snake
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
