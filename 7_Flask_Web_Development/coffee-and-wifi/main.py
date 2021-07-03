bc466a1dff1721528becfb9b1ce70e2b1bbb5328
Dayeon Lee <iamdayeonlee@gmail.com>
2021-07-03 21:00:16 +0900

    Created a free wifi available cafes website with Flask, WTForms and CSV file

--- 7_Flask_Web_Development/coffee-and-wifi/main.py
+++ 7_Flask_Web_Development/coffee-and-wifi/main.py
@@ -5,11 +5,9 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '-'
 Bootstrap(app)
-
 
 class CafeForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -57,23 +55,17 @@ class CafeForm(FlaskForm):
                             '🔌🔌🔌🔌🔌'])
     submit = SubmitField('Submit')
 
-
-
-
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
 #e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
 
 # all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route('/add', methods=["GET", "POST"])
 def add_cafe():
@@ -90,7 +82,6 @@ def add_cafe():
                            f"{form.wifi.data},"
                            f"{form.power.data}")
         return redirect(url_for('cafes'))
-
     return render_template('add.html', form=form)
 
 
